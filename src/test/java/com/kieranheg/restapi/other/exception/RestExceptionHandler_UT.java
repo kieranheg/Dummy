da13a6cf19236baa98187eff5189609c8b84eadf
Kieran Hegarty <kieranheg@users.noreply.github.com>
2020-04-03 13:05:12 +0100

    Finished GET UTs part deux

--- src/test/java/com/kieranheg/restapi/other/exception/RestExceptionHandler_UT.java
+++ src/test/java/com/kieranheg/restapi/other/exception/RestExceptionHandler_UT.java
@@ -4,18 +4,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 public class RestExceptionHandler_UT {
     
     @Test
+    @DisplayName("Catch any validation Constraint Violation Exception and return Bad Request 400")
+    void givenInvalidParamConstraintViolationExceptionReturnBadRequest()  {
+        Set<ConstraintViolation<?>> violations = new HashSet<>();
+        violations.add(mock(ConstraintViolation.class));
+        ConstraintViolationException constraintViolationException = mock(ConstraintViolationException.class);
+        ResponseEntity actualResponse = new RestExceptionHandler().constraintViolationExceptionHandler(constraintViolationException);
+        
+        assertThat(actualResponse.getStatusCode()).isEqualTo(BAD_REQUEST);
+    }
+    
+    @Test
     @DisplayName("Catch any unhandled Exception and return Internal Server Error 500")
     void givenUnhandledServerExceptionReturnInternalServerError()  {
-        ResponseEntity errorResponse = new ResponseEntity<>(INTERNAL_SERVER_ERROR);
         ResponseEntity actualResponse = new RestExceptionHandler().defaultExceptionHandler(new Exception());
         
         assertThat(actualResponse.getStatusCode()).isEqualTo(INTERNAL_SERVER_ERROR);
-        assertThat(actualResponse.getStatusCode()).isEqualTo(errorResponse.getStatusCode());
     }
 }
